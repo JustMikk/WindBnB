73a14b5ff404b39024e33dc25ba1f744706e4420
Mikiyas Hailegebriel <mikiyasbutu@gmail.com>
2024-08-14 20:05:38 -0800

    Created a modal to add a country selection with a map

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,9 @@ import { DiVim } from "react-icons/di";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import { title } from "process";
+import CountrySelect from "../inputs/CountrySelect";
+import Map from "../Map";
+import dynamic from "next/dynamic";
 
 type Props = {};
 
@@ -45,6 +48,12 @@ export default function RentModal({}: Props) {
   });
 
   const category = watch("category");
+  const location = watch("location");
+
+  const Map = useMemo(
+    () => dynamic(() => import("../Map"), { ssr: false }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -99,11 +108,26 @@ export default function RentModal({}: Props) {
     </div>
   );
 
+  if (step === STEPS.LOCATION)
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is your property located?"
+          subtitle="Pick a location that best describes your property"
+        />
+        <CountrySelect
+          onChange={(value) => setCustomValue("location", value)}
+          value={location}
+        />
+        <Map center={location?.latlng} />
+      </div>
+    );
+
   return (
     <Modal
       isOpen={rentmodal.isOpen}
       onClose={rentmodal.onClose}
-      onSubmit={rentmodal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       body={bodyContent}
       secondaryActionLabel={secondaryActionLabel}
